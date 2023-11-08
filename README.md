80d5d12f62c3dd038929e48df6cb6663f7f49709
stephiioo <stephiioo@gmail.com>
2023-11-08 12:23:53 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # flask_6_api_management
+
+## objective: to develop and document APIs using flask and manage azure API management
+
+### 1. flask-based RESTful API
+
+#### - use flask to create an em=ndpoint that can handle a simple Get request and the response should be in JSON format
+
+#### - link: http://127.0.0.1:5000/hello?name=Stephanie&lastname=Ogbebor
+
+
+### 2. Azure API deployment
+
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return f'Hello from my Flask API Endpoint Server'
+
+@app.route('/hello', methods=['GET'])
+def hello_get():
+    name = request.args.get('name', 'World')
+    lastname = request.args.get('lastname', 'no last name provided')
+    return f'Hello {name} {lastname}!'
+
+@app.route('/hello', methods=['POST'])
+def hello_post():
+    data = request.get_json()
+    if data is None:
+        return jsonify({'error': 'Invalid JSON'}), 400
+    
+    name = data.get('name', 'World')
+    return jsonify({'message': f'Hello {name}!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
